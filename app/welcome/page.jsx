c30ef96b5a5591cff8a76bcaf8660310a2419ca7
suheb <suhebism@gmail.com>
2024-10-20 04:06:51 +0530

    welcome page on progress

--- app/welcome/page.jsx
+++ app/welcome/page.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import Image from 'next/image'
-import bird from '@/public/img/bird.png';
+import WelcomeSlider from '@/components/WelcomeSlider';
+import Link from 'next/link';
 
 const page = () => {
   return (
-    <div className='bg-[#75BC7B] h-screen w-full max-w-sm mx-auto  flex flex-col items-center gap-5'>
-        <div className='flex flex-col items-center gap-10 mt-32 px-5'>
-            <h1 className='text-lg tex-white font-bold text-white text-center'>Do you have knowledge
-            about investment?</h1>
-            <Image src={bird} width={100} height={100}/>
-            <div className='h-1 bg-slate-700 w-8'></div>
-        </div>
-        <div className='h-1/2 bg-[#161515] w-full rounded-t-3xl py-10'>
+    <div className='bg-[#75BC7B] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-screen w-full max-w-sm mx-auto flex flex-col items-center gap-5'>
+      <div className='h-1/2 flex items-center'>
+      <WelcomeSlider />
+
+      </div>
+        
+        <div className='h-1/2 bg-[#161515] w-full rounded-t-3xl py-10 flex flex-col gap-10 items-center'>
         <h1 className='text-lg tex-white font-bold text-white text-center'>Welcome, Future IAS</h1>
+        <div className='flex gap-6'>
+          <Link href='/login' className='h-12 w-32 flex items-center justify-center rounded-full font-medium bg-[#75BC7B]'>Login</Link>
+          <Link href='/signup' className='h-12 w-32 flex items-center justify-center rounded-full font-medium bg-white'>Signup</Link>
+        </div>
         </div>
     </div>
   )
---
+++ components/WelcomeSlider.jsx
@@ -0,0 +1,83 @@
+// components/Slider.js
+"use client";
+import { useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import Image from "next/image";
+const banners = [
+  {
+    id: 1,
+    text: "Welcome to our website!",
+    img: "/img/bird.png",
+  },
+  {
+    id: 2,
+    text: "Discover amazing features!",
+    img: "/img/bird.png",
+  },
+  {
+    id: 3,
+    text: "Join us today!",
+    img: "/img/bird.png",
+  },
+];
+
+const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextBanner = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+  };
+
+  const prevBanner = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? banners.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToBanner = (index) => {
+    setCurrentIndex(index);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: nextBanner,
+    onSwipedRight: prevBanner,
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
+
+  return (
+    <div className="relative w-full max-w-lg mx-auto px-3 flex flex-col gap-36" {...handlers}>
+      <div
+        className="flex transition-transform duration-500"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {banners.map((banner) => (
+          <div
+            key={banner.id}
+            className="min-w-full flex flex-colcursor-pointer select-none"
+          >
+            <div className="mt-20 h-20 w-full p-4 gap-6 rounded-lg flex flex-col items-center justify-between">
+              <h1 className="text-black text-lg font-black">{banner.text}</h1>
+              <Image width={100} height={100} src={banner.img} />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center mt-20">
+        {banners.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => goToBanner(index)}
+            className={`mx-1 w-1 h-1 rounded-full transition-all ease-in-out duration-300 ${
+              index === currentIndex
+                ? "min-w-[60px] h-1 bg-gray-300 transition-all ease-in-out duration-300"
+                : "w-1 bg-gray-300 transition-all ease-in-out duration-300"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
